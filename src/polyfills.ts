348054182d36c5722803baa54cd5a91c761cf34e
Félix Pelletier <felix.pelletier.01@gmail.com>
2019-01-31 23:09:22 -0500

    - Installed process; - Added process as a property of a window in polyfills.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import * as process from "process";
+
+(window as any).process = process;
